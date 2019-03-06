a1acfcfd75d41d444d8beea7286aea52c5a3ccc3
D7703_15 <d7703_15@d7703-15ui-imac.local>
2019-03-06 16:18:31 +0900

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by D7703_15 on 2019. 3. 6..
+//  Copyright © 2019년 BlueScreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue
+    }
+
+
+}
+
